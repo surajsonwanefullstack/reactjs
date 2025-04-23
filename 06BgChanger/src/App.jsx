38970811e6c0e552704f44ecbb93f4f96649bf92
Suraj Sonwane <surajsonwanefullstack@gmail.com>
2025-04-23 14:46:44 +0530

    tailwind setup for bgchanger Project

---
+++ 06BgChanger/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="text-3xl">Welcome ReactJs</div>
+    </>
+  )
+}
+
+export default App
