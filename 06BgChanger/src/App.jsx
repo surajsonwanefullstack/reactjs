094fe64fcf9d001209c0eab48efcc25c49d26410
Suraj Sonwane <surajsonwanefullstack@gmail.com>
2025-04-23 14:55:28 +0530

    bgcganger project complete

--- 06BgChanger/src/App.jsx
+++ 06BgChanger/src/App.jsx
@@ -1,14 +1,27 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+const [color,setColor] = useState("blue")
+useEffect(()=>{
+  document.body.style.backgroundColor=color
+},[color])
+useEffect(()=>{
+  document.body.style.backgroundColor=color
+},[])
+
 
   return (
-    <>
-     <div className="text-3xl">Welcome ReactJs</div>
-    </>
+    <div className=' flex justify-around items-center gap-2.5'>
+     <button onClick={()=>setColor("cyan")}>Cyan</button>
+     <button onClick={()=>setColor("pink")}>Pink</button>
+     <button onClick={()=>setColor("red")}>Red</button>
+
+    </div>
   )
 }
 
